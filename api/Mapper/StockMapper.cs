85a0d7c04d2dc76543f88bd49bad57ffa57e000f
Zengoozz <classicalegyptian@gmail.com>
2024-05-21 10:57:30 +0300

    DTOs_Try

---
+++ api/Mapper/StockMapper.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Stock;
+using api.Models;
+
+namespace api.Mapper
+{
+    public static class StockMapper
+    {
+        public static StockDto ToStockDto(this Stock model)
+        {
+            return new StockDto
+            {
+                Id = model.Id,
+                Symbol = model.Symbol,
+                CompanyName = model.CompanyName,
+                Purchase = model.Purchase,
+                LastDiv = model.LastDiv,
+                MarketCap = model.MarketCap
+            };
+        }
+    }
+}
